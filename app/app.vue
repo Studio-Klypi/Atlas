0fae508f9ddbe2b7036ed71afede711b42ee12a9
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-08-27 17:28:07 +0200

    :rocket: add dialogs to main page

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,25 @@
+<script setup lang="ts">
+import UserProfile from "~/components/shared/user/dialogs/UserProfile.vue";
+import UserPreferences from "~/components/shared/user/dialogs/UserPreferences.vue";
+
+const { user, dialogs } = storeToRefs(useUserStore());
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtLoadingIndicator />
     <NuxtLayout />
+
+    <UserProfile
+      v-if="user"
+      v-model:open="dialogs.profile"
+      :user="user"
+    />
+    <UserPreferences
+      v-if="user"
+      v-model:open="dialogs.preferences"
+      :user="user"
+    />
   </div>
 </template>
