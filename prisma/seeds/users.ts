9b2fa63fb06d5ce11ccd7eb5a27c9534a4855d74
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-08-27 11:39:54 +0200

    :rocket: add user seeding

---
+++ prisma/seeds/users.ts
@@ -0,0 +1,33 @@
+import type { PrismaClient } from "@prisma/client";
+import argon2 from "argon2";
+
+const data: {
+  id: string;
+  firstname: string;
+  lastname: string;
+  roles: string[];
+  email: string;
+  password?: string;
+}[] = [
+  {
+    id: "cmetrwumx000104jrhpqu4t16",
+    firstname: "Root",
+    lastname: "Root",
+    roles: ["admin", "moderator", "writer", "support", "accountant", "member"],
+    email: "contact@studio-klypi.com",
+  },
+];
+
+export async function seedUsers(client: PrismaClient) {
+  for (const user of data)
+    await client.user.upsert({
+      where: {
+        id: user.id,
+      },
+      update: {},
+      create: {
+        ...user,
+        password: user.password ?? await argon2.hash(process.env.ROOT_PASSWORD ?? "root"),
+      },
+    });
+}
