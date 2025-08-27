e1ab686d9654dc8a40c572bdb911b1520bdab55a
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-08-27 15:29:25 +0200

    :hammer: update root password

--- prisma/seeds/users.ts
+++ prisma/seeds/users.ts
@@ -1,11 +1,12 @@
 import type { PrismaClient } from "@prisma/client";
 import argon2 from "argon2";
+import type { ERole } from "../../shares/types/user";
 
 const data: {
   id: string;
   firstname: string;
   lastname: string;
-  roles: string[];
+  roles: ERole[];
   email: string;
   password?: string;
 }[] = [
@@ -24,7 +25,10 @@ export async function seedUsers(client: PrismaClient) {
       where: {
         id: user.id,
       },
-      update: {},
+      update: {
+        ...user,
+        password: user.password ?? await argon2.hash(process.env.ROOT_PASSWORD ?? "root"),
+      },
       create: {
         ...user,
         password: user.password ?? await argon2.hash(process.env.ROOT_PASSWORD ?? "root"),
