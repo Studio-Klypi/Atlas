1baef1e5780f17318208e4f61b96721c2226853a
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-08-28 00:10:22 +0200

    :rocket: setup default client and contact models entities

---
+++ prisma/models/contact.ts
@@ -0,0 +1,11 @@
+export async function create() {}
+
+export async function update() {}
+
+export async function archive() {}
+
+export async function destroy() {}
+
+export async function findOne() {}
+
+export async function findAll() {}
