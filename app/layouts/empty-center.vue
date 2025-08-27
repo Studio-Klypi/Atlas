0c25afe9db05b821d34946a3ff80f2081fce100c
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-08-27 10:13:19 +0200

    :rocket: setup default front page, layout and middleware

---
+++ app/layouts/empty-center.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Layout from "~/components/shared/primitives/layout/Layout.vue";
+</script>
+
+<template>
+  <Layout
+    name="empty-center"
+    class="min-h-dvh w-full grid place-items-center px-6 py-4"
+  >
+    <NuxtPage />
+  </Layout>
+</template>
---
+++ app/pages/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Page from "~/components/shared/primitives/layout/Page.vue";
+
+definePageMeta({
+  layout: "empty-center",
+  auth: false,
+});
+</script>
+
+<template>
+  <Page name="login">
+    {{ $t("login.title") }}
+  </Page>
+</template>
