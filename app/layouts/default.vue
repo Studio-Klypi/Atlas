af31f7f532544e72116bf439d41c6bf5fb9813a6
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-08-28 17:43:18 +0200

    :fire: remove layout primitive

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import Layout from "~/components/shared/primitives/layout/Layout.vue";
 import GlobalSidebar from "~/components/shared/navigation/GlobalSidebar.vue";
 
 const { t } = useI18n();
@@ -8,30 +7,25 @@ const pageName = computed(() => t(`${useRoute().meta.displayName}.title`));
 </script>
 
 <template>
-  <Layout>
-    <SidebarProvider>
-      <GlobalSidebar />
+  <SidebarProvider data-layout="default">
+    <GlobalSidebar />
+    <SidebarInset class="min-w-0">
+      <header class="px-4 h-16 data-[state=open]:h-12 flex items-center border-b gap-4">
+        <div class="flex items-center gap-2">
+          <SidebarTrigger />
+          <Separator
+            orientation="vertical"
+            class="h-6!"
+          />
+          <p class="text-sm font-bold">
+            {{ pageName }}
+          </p>
+        </div>
+      </header>
 
-      <SidebarInset>
-        <main class="flex-1">
-          <header class="px-4 h-16 data-[state=open]:h-12 flex items-center border-b gap-4">
-            <div class="flex items-center gap-2">
-              <SidebarTrigger />
-              <Separator
-                orientation="vertical"
-                class="h-6!"
-              />
-              <p class="text-sm font-bold">
-                {{ pageName }}
-              </p>
-            </div>
-          </header>
-
-          <div class="p-4">
-            <NuxtPage />
-          </div>
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
-  </Layout>
+      <div class="p-4">
+        <NuxtPage />
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
 </template>
